93a2baf76f7acb1b9560ffb4ec17ecdb283a9907
izmain <izmain@bk.ru>
2017-04-20 17:33:50 +0300

    result

--- app/src/main/java/net/den/zda3/BaseEdit.java
+++ app/src/main/java/net/den/zda3/BaseEdit.java
@@ -5,20 +5,61 @@ import android.app.*;
 import android.content.*;
 import android.os.*;
 import android.widget.*;
+import android.view.View.*;
+import android.view.*;
 
 public class BaseEdit extends Activity {
 	
-	TextView tvInfo;
+	private static final int RESULT_TIME = 1;
+	String timePicStr ;
 	
-	Intent intnInit;
+	TextView tvInfo,tvData,tvMinHor;
+	Button btTimePic;
+	EditText etTermin;
+	
+	Intent intnInit,intnTimePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_edit);
 		tvInfo=(TextView) findViewById(R.id.tv_info);
+		btTimePic=(Button) findViewById(R.id.bt_time_pick);
+		tvData=(TextView) findViewById(R.id.tv_date);
+		tvMinHor=(TextView) findViewById(R.id.tv_min_hor);
+			
+		initz();
+	}
+
+	private void initz()
+	{
 		intnInit=getIntent();
 		tvInfo.setText("Редактирование задания #"+
 		               (intnInit.getIntExtra("item", -1)+1));
-    }
+		btTimePic.setOnClickListener(new OnClickListener(){
+
+				
+
+				@Override
+				public void onClick(View v)
+				{
+					intnTimePic=new Intent(getApplicationContext(), MainActivity.class);
+					startActivityForResult(intnTimePic,RESULT_TIME);
+					
+				}
+				
+			
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		switch (requestCode) {
+			case RESULT_TIME:
+				tvData.setText(data.getStringExtra("time"));
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
 }
